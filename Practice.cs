eab0ccabed63bdbdf960ca5678b105506423312b
gusrms405 <gusrms4051@gmail.com>
2025-09-29 13:39:48 +0900

    연습문제

--- Practice.cs
+++ Practice.cs
@@ -1,23 +1,28 @@
 using System;
 using System.Collections;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.Versioning;
 class Program
 {
     static void Main()
     {
-        
+        int[][] numbers = new int[3][];
+        numbers[0] = new int[] { 3, 7, 12, 20 };
+        numbers[1] = new int[] { 0, 5, 15 };
+        numbers[2] = new int[] { 8, 9, 14, 18, 2 };
 
-        List<string> names = new List<string>();
-
-        names.Add("젤다");
-        names.Add("리쿵");
-        names.Add("가논");
-
-        foreach (string name in names)
+        for (int i = 0; i < numbers.Length; i++)
         {
-            Console.WriteLine(name);
+            for (int j = 0; j < numbers[i].Length; j++)
+            {
+                if (numbers[i][j] > 10)
+                    Console.WriteLine(numbers[i][j]);
+            }
+
+
         }
         
+
     }
 }
